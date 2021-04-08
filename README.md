dc8f6a6014b31a3090a26ef84c844f8ac8f4cec2
Jake Chamberlain <jake@jchamb.com>
2021-04-08 14:54:50 -0400

    remove data sources and add remoteload

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # referer-parser node.js (JavaScript) library
 
-This is the node.js (JavaScript) implementation of [referer-parser][referer-parser], the library for extracting search marketing data from referer _(sic)_ URLs.
+Updated implementation [referer-parser][referer-parser] that requires you to feed your own referers for parsing instead of loading from within packages.
 
-The implementation uses the shared 'database' of known referers found in [`referers.yml`][referers-yml]
+Provides access to two methods for loading and preparing the referrers object.
+
+This is the node.js (JavaScript) implementation of [referer-parser][referer-parser], the library for extracting search marketing data from referer _(sic)_ URLs.
 
 The Javascript version of referer-parser is maintained by [Martin Katrenik][mkatrenik].
 
@@ -15,44 +17,55 @@ The Javascript version of referer-parser is maintained by [Martin Katrenik][mkat
 Create a new instance of a Referer object by passing in the url you want to parse:
 
 ```js
-var Referer = require('referer-parser')
+const { Referer } = require('referer-parser');
 
-referer_url = 'http://www.google.com/search?q=gateway+oracle+cards+denise+linn&hl=en&client=safari'
+referer_url = 'http://www.google.com/search?q=gateway+oracle+cards+denise+linn&hl=en&client=safari';
 
-var r = new Referer(referer_url)
+const r = new Referer(referer_url, currect_url, referers);
 ```
 
-The `r` variable now holds a Referer instance.  The important attributes are:
+The `r` variable now holds a Referer instance. The important attributes are:
 
 ```js
-console.log(r.known)              // true
-console.log(r.referer)            // 'Google'
-console.log(r.medium)             // 'search'
-console.log(r.search_parameter)   // 'q'
-console.log(r.search_term)        // 'gateway oracle cards denise linn'
-console.log(r.uri)                // result of require('url').parse(...)
-```
-
-Optionally, pass in the current URL as well, to handle internal referers
-
-```js
-var Referer = require('referer-parser')
-
-var referer_url = 'http://www.snowplowanalytics.com/about/team'
-var current_url = 'http://www.snowplowanalytics.com/account/profile'
-
-var r = Referer(referer_url, current_url)
+console.log(r.known); // true
+console.log(r.referer); // 'Google'
+console.log(r.medium); // 'search'
+console.log(r.search_parameter); // 'q'
+console.log(r.search_term); // 'gateway oracle cards denise linn'
+console.log(r.uri); // result of require('url').parse(...)
 ```
 
 The attributes would be
 
 ```js
-console.log(r.known)              // true
-console.log(r.referer)            // null
-console.log(r.medium)             // 'internal'
-console.log(r.search_parameter)   // null
-console.log(r.search_term)        // null
-console.log(r.uri)                // result of require('url').parse(...)
+console.log(r.known); // true
+console.log(r.referer); // null
+console.log(r.medium); // 'internal'
+console.log(r.search_parameter); // null
+console.log(r.search_term); // null
+console.log(r.uri); // result of require('url').parse(...)
+```
+
+Preparing Loading referers
+
+```js
+const { loadReferersRemote } = require('referer-parser');
+
+// Remote source
+// loadReferersRemote(URL, converter=JSON.parse)
+const referrers = loadReferersRemote(
+  'https://s3-eu-west-1.amazonaws.com/snowplow-hosted-assets/third-party/referer-parser/referers-latest.json'
+);
+
+// local source
+// loadReferers(refererObject)
+
+const { loadReferers } = require('referer-parser');
+const path = require('path');
+const fs = require('fs');
+
+const dataFile = fs.readFileSync(path.join(__dirname, 'data', 'referers.json'));
+const referrers = loadReferers(JSON.parse(dataFile));
 ```
 
 ## Copyright and license
@@ -69,8 +82,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
 [referer-parser]: https://github.com/snowplow/referer-parser
-[referers-yml]: https://github.com/snowplow/referer-parser/blob/master/referers.yml
-
 [mkatrenik]: https://github.com/mkatrenik
-
 [license]: http://www.apache.org/licenses/LICENSE-2.0
--- index.js
+++ index.js
@@ -1,132 +1,136 @@
+var https = require('https'),
+  url = require('url'),
+  querystring = require('querystring');
 
-var fs = require('fs')
-  , url = require('url')
-  , querystring = require('querystring')
-  , yaml = require('js-yaml')
-  , path = require('path')
-  ;
+function loadReferers(source) {
+  var referers_dict = {};
 
-module.exports = Referer
+  for (var medium in source) {
+    var conf_list = source[medium];
 
-var dataFile = fs.readFileSync(path.join(__dirname, 'data', 'referers.yml'))
-var REFERERS = loadReferers(yaml.load(dataFile.toString()))
+    for (var referer_name in conf_list) {
+      var config = conf_list[referer_name];
+      var params = null;
 
-function loadReferers (source) {
-  var referers_dict = {}
-
-  for(var medium in source) {
-    var conf_list = source[medium]
-
-    for(var referer_name in conf_list) {
-      var config = conf_list[referer_name]
-      var params = null
-
-      if(config.parameters) {
-        params = config.parameters.map(function(p) { return p.toLowerCase() })
+      if (config.parameters) {
+        params = config.parameters.map(function (p) {
+          return p.toLowerCase();
+        });
       }
-      config.domains.forEach(function(domain){
+      config.domains.forEach(function (domain) {
         referers_dict[domain] = {
-          'name': referer_name,
-          'medium': medium
+          name: referer_name,
+          medium: medium,
+        };
+        if (params) {
+          referers_dict[domain]['params'] = params;
         }
-        if(params){
-          referers_dict[domain]['params'] = params
-        }
+      });
+    }
+  }
+  return referers_dict;
+}
+
+async function loadReferersRemote(url, convert = JSON.parse) {
+  const data = await new Promise((resolve, reject) => {
+    https
+      .get(url, res => {
+        res.on('data', d => resolve(d));
       })
-    }
-  }
-  return referers_dict
+      .on('error', e => {
+        reject(e);
+      });
+
+    loadReferers(convert(data));
+  });
 }
 
-function Referer (referer_url, current_url, referers) {
-  this.known = false
-  this.referer = null
-  this.medium = 'unknown'
-  this.search_parameter = null
-  this.search_term = null
-  this.referers = referers || REFERERS
+function Referer(referer_url, current_url, referers) {
+  this.known = false;
+  this.referer = null;
+  this.medium = 'unknown';
+  this.search_parameter = null;
+  this.search_term = null;
+  this.referers = referers;
 
-  var ref_uri = url.parse(referer_url)
-  var ref_host = ref_uri.hostname
-  this.known = Boolean(~['http:', 'https:'].indexOf(ref_uri.protocol))
-  this.uri = ref_uri
+  var ref_uri = url.parse(referer_url);
+  var ref_host = ref_uri.hostname;
+  this.known = Boolean(~['http:', 'https:'].indexOf(ref_uri.protocol));
+  this.uri = ref_uri;
 
-  if(!this.known) return
+  if (!this.known) return;
 
-  if(current_url){
-    var curr_uri = url.parse(current_url)
-    var curr_host = curr_uri.hostname
+  if (current_url) {
+    var curr_uri = url.parse(current_url);
+    var curr_host = curr_uri.hostname;
 
-    if(curr_host == ref_host) {
-      this.medium = 'internal'
-      return
+    if (curr_host == ref_host) {
+      this.medium = 'internal';
+      return;
     }
   }
 
-  var referer = this._lookup_referer(ref_host, ref_uri.pathname, true)
-  if(!referer){
-    referer = this._lookup_referer(ref_host, ref_uri.pathname, false)
-    if(!referer){
-      this.medium = 'unknown'
-      return
+  var referer = this._lookup_referer(ref_host, ref_uri.pathname, true);
+  if (!referer) {
+    referer = this._lookup_referer(ref_host, ref_uri.pathname, false);
+    if (!referer) {
+      this.medium = 'unknown';
+      return;
     }
   }
 
-  this.referer = referer['name']
-  this.medium = referer['medium']
+  this.referer = referer['name'];
+  this.medium = referer['medium'];
 
-  if(referer['medium'] == 'search') {
-    if(!referer['params']) return
+  if (referer['medium'] == 'search') {
+    if (!referer['params']) return;
 
-    var pqs = querystring.parse(ref_uri.query)
+    var pqs = querystring.parse(ref_uri.query);
 
-    for(var param in pqs) {
-      var val = pqs[param]
+    for (var param in pqs) {
+      var val = pqs[param];
 
-      if(referer['params'].indexOf(param.toLowerCase()) !== -1) {
-        this.search_parameter = param
-        this.search_term = val
+      if (referer['params'].indexOf(param.toLowerCase()) !== -1) {
+        this.search_parameter = param;
+        this.search_term = val;
       }
     }
   }
 }
 
-Referer.prototype._lookup_referer = function(ref_host, ref_path, include_path) {
+Referer.prototype._lookup_referer = function (ref_host, ref_path, include_path) {
   // console.log(ref_host, ref_path, include_path)
-  var referer = null
+  var referer = null;
 
-  if(include_path)
-    referer = this.referers[ref_host + ref_path]
-  else
-    referer = this.referers[ref_host]
-  if(!referer) {
-    if(include_path){
-      var path_parts = ref_path.split('/')
-      if(path_parts.length > 1) {
+  if (include_path) referer = this.referers[ref_host + ref_path];
+  else referer = this.referers[ref_host];
+  if (!referer) {
+    if (include_path) {
+      var path_parts = ref_path.split('/');
+      if (path_parts.length > 1) {
         try {
-          referer = this.referers[ref_host + '/' + path_parts[1]]
-        } catch (e) {
-
-        }
+          referer = this.referers[ref_host + '/' + path_parts[1]];
+        } catch (e) {}
       }
     }
   }
 
-  if(!referer) {
-    try{
-      var idx = ref_host.indexOf('.')
-      if(idx === -1) return null
+  if (!referer) {
+    try {
+      var idx = ref_host.indexOf('.');
+      if (idx === -1) return null;
 
-      var slicedHost = ref_host.slice(idx + 1)
-      return this._lookup_referer(
-          slicedHost,
-          ref_path, include_path
-      )
+      var slicedHost = ref_host.slice(idx + 1);
+      return this._lookup_referer(slicedHost, ref_path, include_path);
     } catch (e) {
-      return null
+      return null;
     }
-  } else return referer
+  } else return referer;
 };
+
+exports.Referer = Referer;
+exports.loadReferers = loadReferers;
+exports.loadReferersRemote = loadReferersRemote;
 
 // var r = new Referer("http://www.google.com/search?q=gateway+oracle+cards+denise+linn&hl=en&client=safari")
 // console.log(r.uri)
